f5f56dd882d5735a7038528ddbc13c8d31b89cfa
sy0725work@gmail.com <sy0725work@gmail.com@c1abda00-6e9e-4933-64f5-1a0d4a7cf787>
2012-09-24 20:11:44 +0000

    tupian

--- com_wondertek_dw/common/util.lua
+++ com_wondertek_dw/common/util.lua
@@ -14,6 +14,19 @@ function setAllShoworHide(sprite, isShow)
     Sprite:setActive(sprite, isShow)
     Sprite:setEnable(sprite, isShow)
 end
+function getJsonArrayCount(data)
+    local count = 0
+    if data then
+        if  #data == 0 and type(data[0]) == 'table' then
+            count = 1
+        else
+            count = #data + 1
+        end
+    else
+    return 0
+    end
+    return count
+end
 
 function getCurDateAndTime()
     local year = os.date("*t")["year"]
@@ -73,4 +86,47 @@ function Split(szFullString, szSeparator)
         nSplitIndex = nSplitIndex + 1
     end
     return nSplitArray
+end
+
+function getErrorCode(code)
+    -- body
+    if code == '0' then
+        return '成功'
+        elseif code == '-1' then
+        return '接口内部发生异常'
+        elseif code == '10' then
+        return '用户不存在'
+        elseif code == '11' then
+        return '用户密码错误'
+        elseif code == '12' then
+        return '用户被禁用'
+        elseif code == '13' then
+        return '用户手机号码不正确（需做手机绑定）'
+        elseif code == '14' then
+        return '用户暂无权限'
+        elseif code == '20' then
+        return '没有待办事项'
+        elseif code == '21' then
+        return '待办处理失败'
+        elseif code == '22' then
+        return '巡检组不存在'
+        elseif code == '23' then
+        return '待办转派失败'
+        elseif code == '24' then
+        return '转派人不存在'
+        elseif code == '30' then
+        return '图片上传失败'
+        elseif code == '40' then
+        return '更新失败'
+        elseif code == '50' then
+        return '数据不存在'
+        elseif code == '80' then
+        return 'Cmd请求不合法（未知请求）'
+        elseif code == '81' then
+        return '请求参数有误'
+        elseif code == '90' then
+        return 'Xml解析异常'
+    else
+        return '未知错误'
+    end
 end
