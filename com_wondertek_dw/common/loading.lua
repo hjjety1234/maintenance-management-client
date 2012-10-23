6b338592dc8f2db6edb73ec2f05958e923b3c002
chaobingliu@gmail.com <chaobingliu@gmail.com@c1abda00-6e9e-4933-64f5-1a0d4a7cf787>
2012-10-23 02:58:28 +0000

    新增loaing百分比效果。

--- com_wondertek_dw/common/loading.lua
+++ com_wondertek_dw/common/loading.lua
@@ -25,6 +25,7 @@ Loading.propTable = {
     frame9 = {rect='0,0,81,81', src='WONDER:\\framework\\image\\loading.png', style='autosize', rotate='90', extendstyle='1111'},
     frame10 = {rect='0,0,81,81', src='WONDER:\\framework\\image\\loading.png', style='autosize', rotate='60', extendstyle='1111'},
     frame11 = {rect='0,0,81,81', src='WONDER:\\framework\\image\\loading.png', style='autosize', rotate='30', extendstyle='1111'},
+    processLbl = {rect='200,368,81,81', text='0', extendstyle='1111'},
     clickBtn = {rect='430,750,49,49', OnSelect='_loadingClose', text='取消', color="#FFFFFF",
                 normal = 'src:WONDER:\\framework\\image\\button.png;style:sudoku-auto;sudoku:4,4,4,4',
                 focus = 'src:WONDER:\\framework\\image\\button_f.png;style:sudoku-auto;sudoku:4,4,4,4',
@@ -35,4 +36,38 @@ Loading.propTable = {
 -- Loading:isShow() 判断是否show（显示），已显示返回true否则false
 -- Loading:close() 关闭Loading
 
+Loading._show = Loading.show
+function Loading:show(rootSprite)
+    Loading:_show(rootSprite)
+    Timer:set(1234, 500, 'loadingProcess')
+end
 
+function loadingProcess(idEvent)
+    local processLbl = Sprite:findChild(Sprite:getCurScene(), 'processLbl')
+    local curNum = tonumber(Sprite:getText(processLbl))
+    if curNum >= 100 then
+        Sprite:setProperty(processLbl, 'text', '100')
+    else
+        math.random()
+        local num = math.random(5)+curNum
+        if num >= 100 then
+            Sprite:setProperty(processLbl, 'text', '100')
+        else
+            Sprite:setProperty(processLbl, 'text', tostring(num))
+            Timer:set(1234, 1000, 'loadingProcess')
+        end
+    end
+end
+
+Loading._close = Loading.close
+function Loading:close(loadState)
+    Timer:cancel(1234)
+    local processLbl = Sprite:findChild(Sprite:getCurScene(), 'processLbl')
+    Sprite:setProperty(processLbl, 'text', '100')
+    Timer:set(processLbl, 500, 'loadingClose')
+end
+
+function loadingClose(processLbl)
+    Sprite:setProperty(processLbl, 'text', '0')
+    Loading:_close()
+end
