c5b631f967d29b88ffc26d63dd5ca5649a1647f0
hewu2008@gmail.com <hewu2008@gmail.com@c1abda00-6e9e-4933-64f5-1a0d4a7cf787>
2012-12-13 08:42:19 +0000

    修改待办工单链接

--- com_wondertek_dw/common/defines.lua
+++ com_wondertek_dw/common/defines.lua
@@ -78,6 +78,8 @@ Alias = {
     m_xunjiantijiao = 'MODULE:\\com_wondertek_dw\\m_xunjiantijiao.wdml', -- 巡检项提交页面
     m_notice = 'MODULE:\\com_wondertek_dw\\m_notice.wdml', -- 公告列表(江峰)
     m_noticeDetail = 'MODULE:\\com_wondertek_dw\\m_noticeDetail.wdml', -- 公告详情（江峰）
+    m_daibandetail='MODULE:\\com_wondertek_dw\\m_daibandetail.wdml', -- 待办详情
+    m_yibandetail = 'MODULE:\\com_wondertek_dw\\m_yibandetail.wdml', -- 待办详情
     
 }
 
