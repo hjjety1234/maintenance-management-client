bdaf5c7c8f2e715411b1c2daa22f1ac563905426
hewu2008@gmail.com <hewu2008@gmail.com@c1abda00-6e9e-4933-64f5-1a0d4a7cf787>
2012-12-21 07:36:31 +0000

    s3基线代码

---
+++ com_wondertek_dw_s3/common/defines.lua
@@ -0,0 +1,101 @@
+﻿-- -----------------------------------------------------------------------------
+-- | WonderTek [ 网络无处不在，沟通及时到达 ]
+-- -----------------------------------------------------------------------------
+-- | Copyright (c) 2012, WonderTek, Inc. All Rights Reserved.
+-- -----------------------------------------------------------------------------
+-- | Author: xxxx <xxxx@xxxx.com>
+-- -----------------------------------------------------------------------------
+-- @class Defines - Msg、 Reg、 Key
+-- @note  常量注册列表
+-- -----------------------------------------------------------------------------
+
+Reg.com_wondertek_dw = {
+    config = 'com_wondertek_dw_config', -- 配置文件数据仓库
+    index = 'com_wondertek_dw_index', -- 入口场景数据仓库标识
+    home = 'com_wondertek_dw_home', -- 首页场景数据仓库标识
+    xunjianzhandianDetail = 'com_wondertek_dw_xunjianzhandianDetail' -- 巡检站点页面数据仓库
+}
+
+Regs = {
+    regName = 'com_wondertek_dw',
+}
+
+Alias = {
+    index = 'MODULE:\\com_wondertek_dw\\index.wdml', -- 入口页
+    home = 'MODULE:\\com_wondertek_dw\\home.wdml', -- 首页
+    login = 'MODULE:\\com_wondertek_dw\\login.wdml', -- 登录
+    m_login = 'MODULE:\\com_wondertek_dw\\m_login.wdml', -- 登录
+    xunjianEdit = 'MODULE:\\com_wondertek_dw\\xunjianEdit.wdml', -- 首页
+    xunjianDetail = 'MODULE:\\com_wondertek_dw\\xunjianDetail.wdml', -- 已巡检任务详情
+    radioSample = 'MODULE:\\com_wondertek_dw\\radioSample.wdml', -- 首页 none
+    yinhuanList = 'MODULE:\\com_wondertek_dw\\yinhuanList.wdml', -- 隐患列表
+    yinhuanshangchuan = 'MODULE:\\com_wondertek_dw\\yinhuanshangchuan.wdml', -- 隐患上报
+    m_daibandetail='MODULE:\\com_wondertek_dw\\m_daibandetail.wdml', -- 待办详情
+    m_daibangongdan = 'MODULE:\\com_wondertek_dw\\m_daibangongdan.wdml', -- 待办列表
+    xunjianrenwu = 'MODULE:\\com_wondertek_dw\\xunjianrenwu.wdml', -- 巡检列表
+    search = 'MODULE:\\com_wondertek_dw\\search.wdml', -- 资料搜索
+    searchDetail = 'MODULE:\\com_wondertek_dw\\searchDetail.wdml', -- 资料搜索详情 none
+    xunjianEdit = 'MODULE:\\com_wondertek_dw\\xunjianEdit.wdml', -- 巡检上报
+    notice = 'MODULE:\\com_wondertek_dw\\notice.wdml', -- 公告列表
+    noticeDetail = 'MODULE:\\com_wondertek_dw\\noticeDetail.wdml', -- 公告详情
+    noticeFav = 'MODULE:\\com_wondertek_dw\\noticeFav.wdml', -- 公告详情
+    cheliangguanli = 'MODULE:\\com_wondertek_dw\\m_cheliangguanli.wdml', -- 车辆管理
+    cheliangxiangqing = 'MODULE:\\com_wondertek_dw\\m_cheliangxiangqing.wdml', -- 车辆详情
+    imageDetail = 'MODULE:\\com_wondertek_dw\\imageDetail.wdml', -- 图片详情
+    orignImg = 'MODULE:\\com_wondertek_dw\\orignImg.wdml', -- 查看原图（隐患上传）
+    tongjiliebiao = 'MODULE:\\com_wondertek_dw\\tongjiliebiao.wdml', -- 统计列表
+    tongxunlu='MODULE:\\com_wondertek_dw\\tongxunlu.wdml', -- 通讯录
+    tongxunluDetail='MODULE:\\com_wondertek_dw\\tongxunluDetail.wdml', -- 通讯录详情
+    gongdandetail='MODULE:\\com_wondertek_dw\\gongdandetail.wdml', -- 待办详情
+    gongdan = 'MODULE:\\com_wondertek_dw\\gongdan.wdml', -- 工单列表
+    xunjianSubmit='MODULE:\\com_wondertek_dw\\xunjianEditEx.wdml', -- 工单列表
+    setting = 'MODULE:\\com_wondertek_dw\\setting.wdml', -- 系统设置页面
+    xunjianTask = 'MODULE:\\com_wondertek_dw\\xunjiantaskDetail.wdml', -- 巡检处理 none
+    yinhuanDetail = 'MODULE:\\com_wondertek_dw\\yinhuanDetail.wdml', -- 系统设置页面
+    cardispatch = 'MODULE:\\com_wondertek_dw\\m_carDispatch.wdml', -- 车辆申请
+    dateDialog = 'MODULE:\\com_wondertek_dw\\dateDialog.wdml', -- 日期选择
+    m_yibandetail = 'MODULE:\\com_wondertek_dw\\m_yibandetail.wdml', -- 已办详情
+    jump = 'MODULE:\\com_wondertek_dw\\Jump.wdml', -- 跳转页
+    xunjianjihua = 'MODULE:\\com_wondertek_dw\\xunjianjihua.wdml', -- 巡检计划
+    xunjianzhandian = 'MODULE:\\com_wondertek_dw\\xunjianzhandian.wdml', -- 巡检站点
+    xunjianEditEx = 'MODULE:\\com_wondertek_dw\\xunjianEditEx.wdml', -- 巡检子项列表
+    xunjianupload = 'MODULE:\\com_wondertek_dw\\xunjianupload.wdml', -- 巡检上传
+    xunjianzhandianDetail = 'MODULE:\\com_wondertek_dw\\xunjianzhandianDetail.wdml', -- 巡检站点详情页面
+    xunjiantijiao = 'MODULE:\\com_wondertek_dw\\xunjiantijiao.wdml', -- 巡检项提交页面
+    chart = 'MODULE:\\com_wondertek_dw\\chart.wdml', -- 统计报表demo
+    
+    ------------------------------Metro Style ------------------------------------------
+    home_new = 'MODULE:\\com_wondertek_dw\\home_new.wdml',  -- 首页
+    m_login = 'MODULE:\\com_wondertek_dw\\m_login.wdml',    -- 登录
+    m_daibangongdan = 'MODULE:\\com_wondertek_dw\\m_daibangongdan.wdml', -- 待办列表
+    m_xunjianjihua = 'MODULE:\\com_wondertek_dw\\m_xunjianjihua.wdml', -- 巡检计划
+    m_yinhuanList = 'MODULE:\\com_wondertek_dw\\m_yinhuanList.wdml', -- 隐患列表
+    m_yinhuanshangchuan = 'MODULE:\\com_wondertek_dw\\m_yinhuanshangchuan.wdml', -- 隐患上报
+    m_yinhuanDetail = 'MODULE:\\com_wondertek_dw\\m_yinhuanDetail.wdml', -- 隐患详情
+    m_cardispatch = 'MODULE:\\com_wondertek_dw\\m_carDispatch.wdml', -- 车辆申请
+    m_cheliangguanli = 'MODULE:\\com_wondertek_dw\\m_cheliangguanli.wdml', -- 车辆管理
+    m_daibangongdan = 'MODULE:\\com_wondertek_dw\\m_daibangongdan.wdml', -- 待办列表
+    m_xunjianzhandian = 'MODULE:\\com_wondertek_dw\\m_xunjianzhandian.wdml', -- 巡检站点
+    m_xunjianEditEx = 'MODULE:\\com_wondertek_dw\\m_xunjianEditEx.wdml', -- 巡检子项列表
+    m_xunjiantijiao = 'MODULE:\\com_wondertek_dw\\m_xunjiantijiao.wdml', -- 巡检项提交页面
+    m_xunjianupload = 'MODULE:\\com_wondertek_dw\\m_xunjianupload.wdml', -- 巡检上传
+    m_notice = 'MODULE:\\com_wondertek_dw\\m_notice.wdml', -- 公告列表(江峰)
+    m_noticeDetail = 'MODULE:\\com_wondertek_dw\\m_noticeDetail.wdml', -- 公告详情（江峰）
+    m_noticeFav = 'MODULE:\\com_wondertek_dw\\m_noticeFav.wdml', -- 收藏列表（江峰）
+    m_daibandetail='MODULE:\\com_wondertek_dw\\m_daibandetail.wdml', -- 待办详情
+    m_yibandetail = 'MODULE:\\com_wondertek_dw\\m_yibandetail.wdml', -- 待办详情
+    m_gongdanliucheng = 'MODULE:\\com_wondertek_dw\\m_gongdanliucheng.wdml', -- 工单流程
+    m_tongxunlu = 'MODULE:\\com_wondertek_dw\\m_tongxunlu.wdml', -- 通讯录（江峰）
+    m_tongxunluDetail = 'MODULE:\\com_wondertek_dw\\m_tongxunluDetail.wdml', -- 通讯录（江峰）
+    
+    home_sheng = 'MODULE:\\com_wondertek_dw\\home_sheng.wdml',   -- 省公司首页
+    m_kaohetongji = 'MODULE:\\com_wondertek_dw\\m_kaohetongji.wdml',  -- 考核统计
+    m_kaohebaobiao = 'MODULE:\\com_wondertek_dw\\m_kaohebaobiao.wdml', -- 考核报表
+    m_staff_statistical = 'MODULE:\\com_wondertek_dw\\m_staff_statistical.wdml', -- 人员资质统计
+    m_search = 'MODULE:\\com_wondertek_dw\\m_search.wdml',--资源查询
+    
+    m_xianluxunjian = 'MODULE:\\com_wondertek_dw\\m_xianluxunjian.wdml',--线路巡检
+    
+    m_setting = 'MODULE:\\com_wondertek_dw\\m_setting.wdml', -- 系统设置页面(江峰)
+}
+
---
+++ com_wondertek_dw_s3/common/loading.lua
@@ -0,0 +1,69 @@
+-- -----------------------------------------------------------------------------
+-- | WonderTek [ 网络无处不在，沟通及时到达 ]
+-- -----------------------------------------------------------------------------
+-- | Copyright (c) 2012, WonderTek, Inc. All Rights Reserved.
+-- -----------------------------------------------------------------------------
+-- | Author: xxxx <xxxx@xxxx.com>
+-- -----------------------------------------------------------------------------
+-- | Desc: 等待提示
+-- -----------------------------------------------------------------------------
+
+ -- @brief Loading所有属性
+Loading.propTable = {
+    shadow = {rect='0,0,480,800', alpha='128', color='#000000', extendstyle='1111'},
+    loadingNullBtn = {rect='0,0,480,800', OnSelect='_loadingNullBtnOnSelect', extendstyle='1111'},
+    loadingRect ={ rect='210,370,60,60', delay='12', loop='true', extendstyle='1111'},
+    frame0 = {rect='0,0,60,60', src='file://image/loading_new.png', style='autosize', rotate='360', extendstyle='1111'},
+    frame1 = {rect='0,0,60,60', src='file://image/loading_new.png', style='autosize', rotate='330', extendstyle='1111'},
+    frame2 = {rect='0,0,60,60', src='file://image/loading_new.png', style='autosize', rotate='300', extendstyle='1111'},
+    frame3 = {rect='0,0,60,60', src='file://image/loading_new.png', style='autosize', rotate='270', extendstyle='1111'},
+    frame4 = {rect='0,0,60,60', src='file://image/loading_new.png', style='autosize', rotate='240', extendstyle='1111'},
+    frame5 = {rect='0,0,60,60', src='file://image/loading_new.png', style='autosize', rotate='210', extendstyle='1111'},
+    frame6 = {rect='0,0,60,60', src='file://image/loading_new.png', style='autosize', rotate='180', extendstyle='1111'},
+    frame7 = {rect='0,0,60,60', src='file://image/loading_new.png', style='autosize', rotate='150', extendstyle='1111'},
+    frame8 = {rect='0,0,60,60', src='file://image/loading_new.png', style='autosize', rotate='120', extendstyle='1111'},
+    frame9 = {rect='0,0,60,60', src='file://image/loading_new.png', style='autosize', rotate='90', extendstyle='1111'},
+    frame10 = {rect='0,0,60,60', src='file://image/loading_new.png', style='autosize', rotate='60', extendstyle='1111'},
+    frame11 = {rect='0,0,60,60', src='file://image/loading_new.png', style='autosize', rotate='30', extendstyle='1111'},
+    processLbl = {rect='210,370,60,60', text='100', extendstyle='1111'},
+    clickBtn = {rect='430,750,49,49', OnSelect='_loadingClose', text='取消', color="#FFFFFF",
+                normal = 'src:WONDER:\\framework\\image\\button.png;style:sudoku-auto;sudoku:4,4,4,4',
+                focus = 'src:WONDER:\\framework\\image\\button_f.png;style:sudoku-auto;sudoku:4,4,4,4',
+                border='1', enable='false', visible='false', extendstyle='1111'}
+}
+
+-- Loading:show(rootSprite) 显示loading
+-- Loading:isShow() 判断是否show（显示），已显示返回true否则false
+-- Loading:close() 关闭Loadingw
+
+Loading._show = Loading.show
+function Loading:show(rootSprite)
+    Loading:_show(rootSprite)
+    Timer:set(1234, 500, 'loadingProcess')
+end
+
+function loadingProcess(idEvent)
+    local processLbl = Sprite:findChild(Sprite:getCurScene(), 'processLbl')
+    local curNum = tonumber(Sprite:getText(processLbl))
+    math.random()
+    local num = curNum - math.random(5)
+    if num < 3 then
+        Sprite:setProperty(processLbl, 'text', '3')
+    else
+        Sprite:setProperty(processLbl, 'text', tostring(num))
+        Timer:set(1234, 1000, 'loadingProcess')
+    end
+end
+
+Loading._close = Loading.close
+function Loading:close(loadState)
+    Timer:cancel(1234)
+    local processLbl = Sprite:findChild(Sprite:getCurScene(), 'processLbl')
+    Sprite:setProperty(processLbl, 'text', '0')
+    Timer:set(processLbl, 500, 'loadingClose')
+end
+
+function loadingClose(processLbl)
+    Sprite:setProperty(processLbl, 'text', '0')
+    Loading:_close()
+end
