72848aa3a97b561e8b8ec0bc80c2215200f24f16
hewu2008 <hewu2008@c1abda00-6e9e-4933-64f5-1a0d4a7cf787>
2013-05-27 03:50:43 +0000

    新增删除本机通话记录的接口

--- Venus_Jttxl/src/com/wondertek/video/luatojava/ClutterLuaContent.java
+++ Venus_Jttxl/src/com/wondertek/video/luatojava/ClutterLuaContent.java
@@ -3,7 +3,11 @@ package com.wondertek.video.luatojava;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.content.ContentResolver;
+import android.provider.CallLog;
+
 import com.wondertek.video.Util;
+import com.wondertek.video.VenusActivity;
 import com.wondertek.video.luatojava.base.LuaContent;
 import com.wondertek.video.luatojava.base.LuaResult;
 
@@ -12,6 +16,8 @@ public class ClutterLuaContent extends LuaContent {
 	private static final String ACTION_CADD = "CAdd";
 	private static final String ACTION_CCBTEST = "CCBTest";
 	private static final String ACTION_CCBTESTTWO = "CCBTestTwo";
+	private static final String ACTION_DELETECALLCONTENT = "DeleteCallContent";
+	private static final String ACTION_DELETE_CALLLOG = "deleteCallLog";
 	private static ClutterLuaContent instance = null;
 
 	public static ClutterLuaContent getInstance()
@@ -53,7 +59,17 @@ public class ClutterLuaContent extends LuaContent {
         	{
         		CCBTestTwo(args.getString(0), args.getString(1), args.getString(2));
         		return null;
-        	}else
+        	}
+        	else if(action.equals(ACTION_DELETECALLCONTENT))
+        	{
+        		DeleteCallContent();
+        		return null;
+        	}
+        	else if(action.equals(ACTION_DELETE_CALLLOG)) 
+        	{
+        		result += deleteCallLog(args.getString(0));
+        	}
+        	else
         	{
         		return new LuaResult(LuaResult.Status.INVALID_ACTION);
         	}
@@ -100,5 +116,27 @@ public class ClutterLuaContent extends LuaContent {
 	public void CCBTestTwo(String str1,String str2,String str3)
 	{
 		Util.Trace("CCBTestTwo =" + str1 + ","+str2+ ","+str3);
+	}
+	
+	public void DeleteCallContent()
+	{
+		Util.Trace("DeleteCallContent");
+		ContentResolver resolver = VenusActivity.appActivity.getContentResolver();
+		resolver.delete(CallLog.Calls.CONTENT_URI, null, null);
+	}
+	
+	public boolean deleteCallLog(String number) {
+		Util.Trace("[deleteCallLog] number: " + number);
+		if (number == null) {
+			return false;
+		}
+		ContentResolver resolver = VenusActivity.appActivity
+				.getContentResolver();
+		int ret = resolver.delete(CallLog.Calls.CONTENT_URI,
+				CallLog.Calls.NUMBER + "=?", new String[] { number });
+		if (ret > 0)
+			return true;
+		else
+			return false;
 	}
 }
