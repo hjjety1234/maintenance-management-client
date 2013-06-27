9896c9cb3d68197fdd880aa7268654c1893aa945
hewu2008 <hewu2008@c1abda00-6e9e-4933-64f5-1a0d4a7cf787>
2013-06-27 13:19:57 +0000

    增加多部门的名片布局

--- Venus_Jttxl/src/com/wondertek/video/caller/FloatRelativeLayout.java
+++ Venus_Jttxl/src/com/wondertek/video/caller/FloatRelativeLayout.java
@@ -1,6 +1,8 @@
 package com.wondertek.video.caller;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -13,63 +15,291 @@ import android.view.ScaleGestureDetector;
 import android.view.ScaleGestureDetector.SimpleOnScaleGestureListener;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.wondertek.jttxl.R;
 
 public class FloatRelativeLayout extends RelativeLayout {
 	private static final String TAG = "FloatRelativeLayout";
-	private static boolean bIsCustomLayout = false;
 	private Context mContext;
+	
+	// 标记是否是自定义的布局
+	private static boolean bIsCustomLayout = false;
+	
+	// 员工信息类
 	private Employee employee;
-	public boolean isScale = false;
-	private float mScaleFactor = 1;
+	
+	public boolean isScale = false;     // 标记是否需要缩放
+	private float mScaleFactor = 1;		// 缩放比例
 	private ScaleGestureDetector mScaleDetector;
-	private float rx;
+	
+	// 触摸点的相对坐标
+	private float rx; 	
 	private float ry;
+	
+	// 触摸点的绝对坐标
+	private float rawX; 
+	private float rawY;
+	
+	// 缩放的中心点坐标
 	private float px;
 	private float py;
-	private float rawX;
-	private float rawY;
-
-	private WindowManager wm = (WindowManager) getContext()
-			.getApplicationContext().getSystemService("window");
-
+	
+	// 系统桌面和布局参数
+	private WindowManager wm = (WindowManager) getContext().getApplicationContext().getSystemService("window");
 	private WindowManager.LayoutParams wmParams = null;
 
+	/**
+	* <p>构造函数，根据条件加载对应的布局 </p>
+	* @param context 
+	* @param params
+	* @param e
+	*/
 	public FloatRelativeLayout(Context context,
 			WindowManager.LayoutParams params, Employee e) {
 		super(context);
+		// 初始化变量
 		employee = e;
 		mContext = context;
 		wmParams = params;
-		mScaleDetector = new ScaleGestureDetector(context,
-				new OnPinchListener());
-		bIsCustomLayout = false;
-		if (employee.getPicutre() != null
-				&& !employee.getPicutre().trim().equals("")) {
+		mScaleDetector = new ScaleGestureDetector(context, new OnPinchListener());
+		
+		// 检查自定义照片是否存在
+		if (employee.getPicutre() != null && !employee.getPicutre().trim().equals("")) {
+			// 如果保存的照片目录不存在，需要先创建
 			File dir = new File(Constants.getLocPicDir());
 			dir.mkdir();
+			
+			// 检查照片文件是否存在
 			String localPicPath = Constants.getLocPicDir() + employee.getPicutre();
 			Log.d(TAG, "[initLayout] local picture path: " + localPicPath);
 			File pic = new File(localPicPath);
 			if (pic.exists() == true) {
-				inflate(context, R.layout.float_view_withphoto, this);
-				bIsCustomLayout = true;
+				showCustomLayout(context);
 			} else {
-				inflate(context, R.layout.float_view, this);
+				// showDefaultLayout(context);
+				showListViewLayout(context);
 			}
 		} else {
-			inflate(context, R.layout.float_view, this);
+			// showDefaultLayout(context);
+			showListViewLayout(context);
 		}
-		initLayout();
 	}
+	
+	/**
+	* @Description 显示默认名片
+	* @author hewu <hewu2008@gmail.com>
+	* @date 2013-6-26 下午6:01:38
+	*/
+	private void showDefaultLayout(Context context) {
+		Log.d(TAG, "[initDefaultLayout]");
+		bIsCustomLayout = false; 
+		inflate(context, R.layout.float_view, this);
 
+		// 显示姓名
+		TextView callerName = (TextView) findViewById(R.id.caller_name);
+		callerName.setText(employee.getName());
+
+		// 显示职位信息
+		TextView headship = (TextView) findViewById(R.id.headship);
+		String defaultHeadship = getResources().getString(R.string.defaultHeadship);
+		if (!employee.getHeadship().trim().equals(""))
+			headship.setText(employee.getHeadship());
+
+		// 显示部门信息
+		TextView dept = (TextView) findViewById(R.id.deptinfo);
+		dept.setText(employee.getDepartment());
+
+		// 显示提示信息
+		showTipText();
+
+		// 如果自定义照片不存在则进行下载
+		if (employee.getPicutre() != null && !employee.getPicutre().trim().equals("")) {
+			File dir = new File(Constants.getLocPicDir());
+			dir.mkdir();
+			String localPicPath = Constants.getLocPicDir() + employee.getPicutre();
+			Log.d(TAG, "[initDefaultLayout] local picture path: " + localPicPath);
+			File pic = new File(localPicPath);
+			if (pic.exists() == false) {
+				Log.d(TAG, "[initLayout] trying to async download picture...");
+				final DownloadFileTask downloadFile = new DownloadFileTask(employee.getPicutre());
+				new Thread() {
+					@Override
+					public void run() {
+						String requestUri = downloadFile.requestResourceUri();
+						if (requestUri != null) {
+							String resourceUri = Constants.RES_PIC_URL_PREFIX + requestUri;
+							downloadFile.execute(resourceUri);
+						} else {
+							Log.w(TAG, "[initLayout] request resource uri failed!");
+						}
+					}
+				}.start();
+			} else {
+				Log.w(TAG, "[initLayout] picture path is null.");
+			}
+		}
+		
+		// 显示自定义LOGO
+		ImageView iv = (ImageView) findViewById(R.id.m_logo);
+		showCustomLogo(iv, employee.getEmpid());
+	}
+	
+	/**
+	* @Description 显示带图片的名片
+	* @author hewu <hewu2008@gmail.com>
+	* @date 2013-6-26 下午5:54:51
+	*/
+	private void showCustomLayout(Context context) {
+		Log.d(TAG, "[initCustomLayout]");
+		bIsCustomLayout = true;
+		inflate(context, R.layout.float_view_withphoto, this);
+		
+		// 显示姓名
+		TextView callerName = (TextView) findViewById(R.id.caller_name);
+		callerName.setText(employee.getName());
+
+		// 显示职位信息
+		TextView headship = (TextView) findViewById(R.id.headship);
+		String defaultHeadship = getResources().getString(R.string.defaultHeadship);
+		if (!employee.getHeadship().trim().equals(""))
+			headship.setText(employee.getHeadship());
+
+		// 显示部门信息
+		TextView dept = (TextView) findViewById(R.id.deptinfo);
+		dept.setText(employee.getDepartment());
+		
+		// 显示自定义照片
+		ImageView bg = (ImageView) findViewById(R.id.incoming_bg);
+		Uri uri = Uri.parse(Constants.getLocPicDir() + employee.getPicutre());
+		bg.setImageURI(uri);
+
+		// 显示联系人信息背景图片
+		ImageView iv = (ImageView) findViewById(R.id.caller_info_bg);
+		iv.setVisibility(VISIBLE);
+		
+		// 显示自定义LOGO
+		ImageView logo = (ImageView)findViewById(R.id.small_logo_icon);
+		showCustomLogo(logo, employee.getEmpid());
+	}
+	
+	/**
+	* @Description 显示多部门的名片
+	* @author hewu <hewu2008@gmail.com>
+	* @date 2013-6-26 下午5:36:19
+	*/
+	private void showListViewLayout(Context context) {
+		inflate(context, R.layout.float_view_listview, this);
+		bIsCustomLayout = true;
+		
+		NonScrollableListView listview = (NonScrollableListView) findViewById(R.id.listView1);
+		listview.setLayout(this);
+		ArrayList<HashMap<String, Object>> listItem = new ArrayList<HashMap<String, Object>>();
+		for(int i = 0; i < 3; i++) {  
+            HashMap<String, Object> map = new HashMap<String, Object>();  
+            map.put("deptname", "安徽移动信息系统部ICT支撑中心");  
+            map.put("headship", "初级研发工程师");  
+            listItem.add(map);  
+        } 
+		
+		SimpleAdapter listItemAdapter = new SimpleAdapter(context, listItem,
+				R.layout.float_view_listitem, 
+				new String[] { "deptname", "headship" }, 
+				new int[] { R.id.deptname, R.id.caller_name });
+		
+		listview.setAdapter(listItemAdapter);
+	}
+	
+	/**
+	 * 显示自定义的公司图标
+	 * @param iv 待显示的ImageView
+	 * @param employeeId 员工编号
+	 */
+	private void showCustomLogo(ImageView iv, String employeeId) {
+		Log.d(TAG, "[showCustomLogo] employee id: " + employeeId);
+		final String logoImg = DbHelper.getLogoImg(employeeId);
+		if (logoImg == null) return;
+		String logoImgPath = Constants.getLocPicDir() + logoImg;
+		File pic = new File(logoImgPath);
+		if (pic.isFile() && pic.exists() == true) {
+			Log.d(TAG, "[showCustomLogo] find custom logo file!");
+			Uri uri = Uri.parse(logoImgPath);
+			iv.setImageURI(uri);
+		}else {
+			Log.d(TAG, "[showCustomLogo] can't find custom logo file, trying to async downloading...!");
+			final DownloadFileTask downloadFile = new DownloadFileTask(logoImg);
+			new Thread() {
+				@Override
+				public void run() {
+					String resourceUri = Constants.RES_LOGO_URL_PREFIX + logoImg;
+					downloadFile.execute(resourceUri);
+				}
+			}.start();
+		}
+	}
+	
+	/**
+	* @Description 第一次显示名片需要显示提示信息
+	* @author hewu <hewu2008@gmail.com>
+	* @date 2013-6-26 下午5:56:20
+	*/
+	public void showTipText() {
+		// get SharedPreferences and editor
+		SharedPreferences popupPos = mContext.getSharedPreferences(
+				PhoneStatReceiver.POPUP_POS, Context.MODE_PRIVATE);
+		TextView tipText = (TextView) findViewById(R.id.tip_text);
+		ImageView iv = (ImageView) findViewById(R.id.tip_bg);
+
+		if (tipText == null || iv == null)
+			return;
+
+		if (popupPos.getBoolean("showTip", true)) {
+			Log.d(TAG, "[showTipText] true");
+			iv.setVisibility(VISIBLE);
+			tipText.setVisibility(VISIBLE);
+		} else {
+			Log.d(TAG, "[showTipText] false");
+			iv.setVisibility(INVISIBLE);
+			tipText.setVisibility(INVISIBLE);
+		}
+	}
+	
+	/**
+	* @Description 设置提示信息的可见性
+	* @author hewu <hewu2008@gmail.com>
+	* @date 2013-6-26 下午5:58:22
+	*/
+	public void setTipTextVisiblity(boolean visibility) {
+		Log.d(TAG, "[setTipTextVisiblity] visibility: " + visibility);
+		// get SharedPreferences and editor
+		SharedPreferences popupPos = mContext.getSharedPreferences(
+				PhoneStatReceiver.POPUP_POS, Context.MODE_PRIVATE);
+		SharedPreferences.Editor editor = popupPos.edit();
+		editor.putBoolean("showTip", visibility);
+		editor.commit();
+		if (visibility == false) {
+			TextView tipText = (TextView) findViewById(R.id.tip_text);
+			ImageView iv = (ImageView) findViewById(R.id.tip_bg);
+			if (tipText == null || iv == null)
+				return;
+			iv.setVisibility(INVISIBLE);
+			tipText.setVisibility(INVISIBLE);
+			
+		}
+	}
+	
+
+	/* (非 Javadoc)
+	* <p> 子视图重绘时调用</p>
+	* @param canvas
+	* @see android.view.ViewGroup#dispatchDraw(android.graphics.Canvas)
+	*/
 	@Override
 	protected void dispatchDraw(Canvas canvas) {
-		Log.d(TAG, "[dispatchDraw] isScale: " + isScale + ", isCustomLayout: "
-				+ bIsCustomLayout);
+		Log.d(TAG, "[dispatchDraw] isScale: " + isScale + ", isCustomLayout: " + bIsCustomLayout);
 		if (bIsCustomLayout == true) {
 			canvas.save(Canvas.MATRIX_SAVE_FLAG);
 			super.dispatchDraw(canvas);
@@ -144,8 +374,10 @@ public class FloatRelativeLayout extends RelativeLayout {
 	}
 
 	/**
-	 * scale the pop's text
-	 */
+	* @Description 对名片的字体进行缩放
+	* @author hewu <hewu2008@gmail.com>
+	* @date 2013-6-26 下午5:55:54
+	*/
 	public void scaleText(float mScaleFactor) {
 		TextView callerName = (TextView) findViewById(R.id.caller_name);
 		TextView headship = (TextView) findViewById(R.id.headship);
@@ -156,57 +388,15 @@ public class FloatRelativeLayout extends RelativeLayout {
 	}
 
 	/**
-	 * show tip text if this it the first time
-	 */
-	public void showTipText() {
-		// get SharedPreferences and editor
-		SharedPreferences popupPos = mContext.getSharedPreferences(
-				PhoneStatReceiver.POPUP_POS, Context.MODE_PRIVATE);
-		TextView tipText = (TextView) findViewById(R.id.tip_text);
-		ImageView iv = (ImageView) findViewById(R.id.tip_bg);
-
-		if (tipText == null || iv == null)
-			return;
-
-		if (popupPos.getBoolean("showTip", true)) {
-			Log.d(TAG, "[showTipText] true");
-			iv.setVisibility(VISIBLE);
-			tipText.setVisibility(VISIBLE);
-		} else {
-			Log.d(TAG, "[showTipText] false");
-			iv.setVisibility(INVISIBLE);
-			tipText.setVisibility(INVISIBLE);
-		}
-	}
-
-	/**
-	 * set tip text visibility
-	 */
-	public void setTipTextVisiblity(boolean visibility) {
-		Log.d(TAG, "[setTipTextVisiblity] visibility: " + visibility);
-		// get SharedPreferences and editor
-		SharedPreferences popupPos = mContext.getSharedPreferences(
-				PhoneStatReceiver.POPUP_POS, Context.MODE_PRIVATE);
-		SharedPreferences.Editor editor = popupPos.edit();
-		editor.putBoolean("showTip", visibility);
-		editor.commit();
-		if (visibility == false) {
-			TextView tipText = (TextView) findViewById(R.id.tip_text);
-			ImageView iv = (ImageView) findViewById(R.id.tip_bg);
-			if (tipText == null || iv == null)
-				return;
-			iv.setVisibility(INVISIBLE);
-			tipText.setVisibility(INVISIBLE);
-			
-		}
-	}
-
+	* @Description 进行名片的缩放
+	* @author hewu <hewu2008@gmail.com>
+	* @date 2013-6-26 下午6:01:04
+	*/
 	public void scale(float scaleFactor, float pivotX, float pivotY) {
 		Log.d(TAG, "[scale] scale:" + scaleFactor + ", pivotX:" + pivotX
 				+ ", pivotY: " + pivotY + ", isScale: " + isScale);
 		if (isScale == false || scaleFactor <= 0.89) {
-			Log.w(TAG,
-					"[scale] isScale is false or reach minmal limit, do nothing...");
+			Log.w(TAG, "[scale] isScale is false or reach minmal limit, do nothing...");
 			return;
 		}
 		if (scaleFactor < 1) {
@@ -220,117 +410,21 @@ public class FloatRelativeLayout extends RelativeLayout {
 		this.invalidate();
 	}
 
+	/**
+	* @Description 设置是否允许缩放
+	* @author hewu <hewu2008@gmail.com>
+	* @date 2013-6-26 下午6:01:26
+	*/
 	public void setScaleEnabled(boolean bEnable) {
 		isScale = bEnable;
 	}
 
-	private void initLayout() {
-		Log.d(TAG, "[initLayout]");
-
-		// set caller name
-		TextView callerName = (TextView) findViewById(R.id.caller_name);
-		callerName.setText(employee.getName());
-
-		// set caller headship
-		TextView headship = (TextView) findViewById(R.id.headship);
-		String defaultHeadship = getResources().getString(
-				R.string.defaultHeadship);
-		if (!employee.getHeadship().trim().equals(""))
-			// && !employee.getHeadship().equals(defaultHeadship))
-			headship.setText(employee.getHeadship());
-
-		// set caller department information
-		TextView dept = (TextView) findViewById(R.id.deptinfo);
-		dept.setText(employee.getDepartment());
-
-		// show tip text if this is the first time
-		showTipText();
-
-		// set caller picture
-		if (employee.getPicutre() != null
-				&& !employee.getPicutre().trim().equals("")) {
-			File dir = new File(Constants.getLocPicDir());
-			dir.mkdir();
-			String localPicPath = Constants.getLocPicDir() + employee.getPicutre();
-			Log.d(TAG, "[initLayout] local picture path: " + localPicPath);
-			File pic = new File(localPicPath);
-			if (pic.exists() == true) {
-				Log.d(TAG, "[initLayout] show custom layout.");
-				initCustomLayout();
-				ImageView iv = (ImageView)findViewById(R.id.small_logo_icon);
-				showCustomLogo(iv, employee.getEmpid());
-			} else {
-				Log.d(TAG, "[initLayout] trying to async download picture...");
-				final DownloadFileTask downloadFile = new DownloadFileTask(
-						employee.getPicutre());
-				new Thread() {
-					@Override
-					public void run() {
-						String requestUri = downloadFile.requestResourceUri();
-						if (requestUri != null) {
-							String resourceUri = Constants.RES_PIC_URL_PREFIX
-									+ requestUri;
-							downloadFile.execute(resourceUri);
-						} else {
-							Log.w(TAG,
-									"[initLayout] request resource uri failed!");
-						}
-					}
-				}.start();
-				ImageView iv = (ImageView)findViewById(R.id.m_logo);
-				showCustomLogo(iv, employee.getEmpid());
-			}
-		} else {
-			Log.w(TAG, "[initLayout] picture path is null.");
-			ImageView iv = (ImageView)findViewById(R.id.m_logo);
-			showCustomLogo(iv, employee.getEmpid());
-		}
-	}
-	
-	/**
-	 * 显示自定义Logo
-	 * @param iv 待显示的ImageView
-	 * @param employeeId 员工编号
-	 */
-	private void showCustomLogo(ImageView iv, String employeeId) {
-		Log.d(TAG, "[showCustomLogo] 员工编号: " + employeeId);
-		final String logoImg = DbHelper.getLogoImg(employeeId);
-		if (logoImg == null) return;
-		// 获取本地图标的路径
-		String logoImgPath = Constants.getLocPicDir() + logoImg;
-		File pic = new File(logoImgPath);
-		if (pic.isFile() && pic.exists() == true) {
-			// 本地LOGO文件存在，直接显示
-			Log.d(TAG, "[showCustomLogo] 本地LOGO文件已找到!");
-			Uri uri = Uri.parse(logoImgPath);
-			iv.setImageURI(uri);
-		}else {
-			// 本地LOGO文件不存在，下载LOGO文件
-			Log.d(TAG, "[showCustomLogo] 本地LOGO文件未找到，尝试下载图标文件!");
-			final DownloadFileTask downloadFile = new DownloadFileTask(logoImg);
-			new Thread() {
-				@Override
-				public void run() {
-					String resourceUri = Constants.RES_LOGO_URL_PREFIX + logoImg;
-					downloadFile.execute(resourceUri);
-				}
-			}.start();
-		}
-	}
-
-	
-	private void initCustomLayout() {
-		Log.d(TAG, "[initCustomLayout]");
-		// change popup window background
-		ImageView bg = (ImageView) findViewById(R.id.incoming_bg);
-		Uri uri = Uri.parse(Constants.getLocPicDir() + employee.getPicutre());
-		bg.setImageURI(uri);
-
-		// set caller info background visible
-		ImageView iv = (ImageView) findViewById(R.id.caller_info_bg);
-		iv.setVisibility(VISIBLE);
-	}
-
+	/* (非 Javadoc)
+	* <p>监听名片的触摸消息 </p>
+	* @param event
+	* @return
+	* @see android.view.View#onTouchEvent(android.view.MotionEvent)
+	*/
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		rawX = event.getRawX();
@@ -369,6 +463,11 @@ public class FloatRelativeLayout extends RelativeLayout {
 		return true;
 	}
 
+	/**
+	* @Description 更新名片的位置
+	* @author hewu <hewu2008@gmail.com>
+	* @date 2013-6-26 下午5:58:55
+	*/
 	private void updateViewPosition() {
 		Log.d(TAG, ">>>updateViewPosition<<<");
 		wmParams.x = (int) (rawX - rx);
@@ -383,6 +482,12 @@ public class FloatRelativeLayout extends RelativeLayout {
 		editor.commit();
 	}
 
+	/**
+	* @Description 缩放的消息监听类
+	* @author hewu <hewu2008@gmail.com>
+	* @date 2013-6-26 下午7:34:33
+	* 
+	*/
 	private class OnPinchListener extends SimpleOnScaleGestureListener {
 		float startingSpan;
 		float startFocusX;
@@ -398,8 +503,7 @@ public class FloatRelativeLayout extends RelativeLayout {
 
 		public boolean onScale(ScaleGestureDetector detector) {
 			Log.d(TAG, ">>>onScale<<<");
-			scale(detector.getCurrentSpan() / startingSpan, startFocusX,
-					startFocusY);
+			scale(detector.getCurrentSpan() / startingSpan, startFocusX, startFocusY);
 			return true;
 		}
 	}
