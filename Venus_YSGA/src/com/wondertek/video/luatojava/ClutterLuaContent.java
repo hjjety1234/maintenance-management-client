69c43398cdf3e1d132a55c0d9adce38ce3ad23eb
hewu2008 <hewu2008@c1abda00-6e9e-4933-64f5-1a0d4a7cf787>
2013-08-01 14:05:47 +0000

    1）新增poi接口；2）集成基地sdk至PJ中

--- Venus_YSGA/src/com/wondertek/video/luatojava/ClutterLuaContent.java
+++ Venus_YSGA/src/com/wondertek/video/luatojava/ClutterLuaContent.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.content.ComponentName;
 import android.content.ContentResolver;
@@ -12,67 +13,96 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.provider.CallLog;
+import android.util.Log;
 
+import com.android.cmcc.Login.SecurityLogin;
+import com.android.cmcc.bean.CmccLocation;
+import com.android.cmcc.bean.LocationParam;
+import com.android.cmcc.listener.CmccLocationListener;
 import com.wondertek.video.Util;
 import com.wondertek.video.VenusActivity;
 import com.wondertek.video.luatojava.base.LuaContent;
 import com.wondertek.video.luatojava.base.LuaResult;
 
-public class ClutterLuaContent extends LuaContent {
+public class ClutterLuaContent extends LuaContent implements CmccLocationListener {
+	private static final String TAG = "ClutterLuaContent";
 	private static final String ACTION_CTRACE = "CTrace";
 	private static final String ACTION_CADD = "CAdd";
 	private static final String ACTION_CCBTEST = "CCBTest";
 	private static final String ACTION_CCBTESTTWO = "CCBTestTwo";
 	private static final String ACTION_DELETECALLCONTENT = "DeleteCallContent";
+	private static final String ACTION_JD_LOCATE = "GetLocationByJD";
 	private static ClutterLuaContent instance = null;
+	private static CmccLocation mCmccLocation = null;
+	private static SecurityLogin mClient = null;
+	private static LocationParam locParam = null;
 
-	public static ClutterLuaContent getInstance()
-	{
-		if(instance == null)
-		{
+	public static ClutterLuaContent getInstance() {
+		if(instance == null) {
 			instance = new ClutterLuaContent();
 		}
-		
 		return instance;
 	}
 	
 	/**
-     * Constructor.
+     * 
      */
     public ClutterLuaContent() {
     	instance = this;
+    	mClient = new SecurityLogin(VenusActivity.appActivity);
+		locParam = new LocationParam();
+		locParam.setServiceId("AHKQXT201307191705");
+		locParam.setOffSet(false);
+		locParam.setLocType("2");
+		mClient.setCmccLocationListener(this);
+		mClient.setLocationParam(locParam);
+		mClient.start();
     }
     
+	/**
+	 * @revised hewu <hewu2008@gmail.com>
+	 * @date 2013/08/01 新增基地定位接口
+	 */
 	@Override
 	public LuaResult execute(String action, JSONArray args) {
-		// TODO Auto-generated method stub
 		LuaResult.Status status = LuaResult.Status.OK;
         String result = "";
-        
         try {
         	if (action.equals(ACTION_CTRACE)) {
         		CTrace(args.getString(0));
         		return null;
-        	}
-        	else if (action.equals(ACTION_CADD)) {
+        	} else if (action.equals(ACTION_CADD)) {
         		result += CAdd(args.getInt(0),args.getInt(1));
-        	}
-        	else if(action.equals(ACTION_CCBTEST))
-        	{
+        	} else if(action.equals(ACTION_CCBTEST)) {
         		result += CCBTest(args.getInt(0),args.getInt(1));
-        	}
-        	else if(action.equals(ACTION_CCBTESTTWO))
-        	{
+        	} else if(action.equals(ACTION_CCBTESTTWO)){
         		CCBTestTwo(args.getString(0), args.getString(1), args.getString(2));
         		return null;
-        	}
-        	else if(action.equals(ACTION_DELETECALLCONTENT))
-        	{
+        	} else if(action.equals(ACTION_DELETECALLCONTENT)) {
         		DeleteCallContent();
         		return null;
-        	}
-        	else
-        	{
+        	}else if (action.equals(ACTION_JD_LOCATE)) {
+        		Log.d(TAG, "[execute] try to get current location...");
+        		mCmccLocation = null;
+        		locParam.setLocType("2");
+    			locParam.setOffSet(false);
+    			mClient.setLocationParam(locParam);
+    			mClient.locCapability();
+    			while (mCmccLocation == null) {
+    				try {
+    					Thread.sleep(500);
+    				} catch (InterruptedException e) {
+    					e.printStackTrace();
+    				}
+    			}
+    			JSONObject obj = new JSONObject();
+    			obj.put("ErrorCode", mCmccLocation.getErrorCode());
+    			obj.put("latitude", mCmccLocation.getLatitude());
+    			obj.put("longitude", mCmccLocation.getLongitude());
+    			obj.put("errRange", mCmccLocation.getErrRange());
+    			obj.put("PosType", mCmccLocation.getPosType());
+    			result = obj.toString();
+        	} else {
         		return new LuaResult(LuaResult.Status.INVALID_ACTION);
         	}
             return new LuaResult(status, result);
@@ -83,20 +113,19 @@ public class ClutterLuaContent extends LuaContent {
 
 	@Override
 	public boolean isSynch(String action) {
-		// TODO Auto-generated method stub
 		if (action.equals(ACTION_CCBTEST)) {
+			return false;
+		}else if (action.equals(ACTION_JD_LOCATE)) {
 			return false;
 		}
 		return true;
 	}
 	
-	public void CTrace(String str)
-	{
+	public void CTrace(String str) {
 		Util.Trace("ClutterLuaContent:CTrace = " + str);
 	}
 	
-	public int CAdd(int a, int b)
-	{
+	public int CAdd(int a, int b) {
 		return a + b;
 	}
 	
@@ -108,7 +137,6 @@ public class ClutterLuaContent extends LuaContent {
 				Thread.sleep(a);
 				Util.Trace("sleep i= " + i + ", time =" + a);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
@@ -120,27 +148,18 @@ public class ClutterLuaContent extends LuaContent {
 		Util.Trace("CCBTestTwo =" + str1 + ","+str2+ ","+str3);
 		Intent sendIntent =new Intent();
 		sendIntent.setAction(Intent.ACTION_VIEW);
-
-//		sendIntent.setData(Uri.parse("file:///mnt/sdcard/3D/TOPGIRL.mp4"));
 		sendIntent.setDataAndType(Uri.parse("file:///mnt/sdcard/3D/TOPGIRL.mp4"), "video/mp4");
-
-//		sendIntent.setType("video/mp4");
-//		sendIntent.setComponent(new ComponentName("com.android.gallery3d", "com.android.gallery3d.app.MovieActivit"));
-//		final PackageManager packageManager = VenusActivity.appActivity.getPackageManager();
-//        List<ResolveInfo> list = packageManager.queryIntentActivities(sendIntent, PackageManager.GET_ACTIVITIES);
-//        if (list.size() > 0) {
-//        	VenusActivity.appActivity.startActivity(Intent.createChooser(sendIntent, "ѡ��Ӧ��"));
-//   
-//        } else {
-//        }
-        
 		VenusActivity.appActivity.startActivity(sendIntent); 
 	}
 	
-	public void DeleteCallContent()
-	{
+	public void DeleteCallContent() {
 		Util.Trace("DeleteCallContent");
 		ContentResolver resolver = VenusActivity.appActivity.getContentResolver();
 		resolver.delete(CallLog.Calls.CONTENT_URI, null, null);
+	}
+
+	@Override
+	public void onReceiveLocation(CmccLocation cmccLocation) {
+		mCmccLocation = cmccLocation;
 	}
 }
