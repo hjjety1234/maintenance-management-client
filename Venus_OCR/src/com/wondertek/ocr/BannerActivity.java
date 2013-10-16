f81eeef1b1368dda69b610e2f3ed2feec65ab55f
hewu2008@gmail.com <hewu2008@gmail.com@c1abda00-6e9e-4933-64f5-1a0d4a7cf787>
2013-10-16 12:03:18 +0000

    更换新首页

--- Venus_OCR/src/com/wondertek/ocr/BannerActivity.java
+++ Venus_OCR/src/com/wondertek/ocr/BannerActivity.java
@@ -1,42 +1,71 @@
 package com.wondertek.ocr;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
 import com.wondertek.banner.BannerLayout;
 import com.wondertek.banner.BannerLayout.OnItemClickListener;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.View;
-import android.widget.Toast;
-
 public class BannerActivity extends Activity {
-
 	BannerLayout bl;
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        
-        bl=(BannerLayout) findViewById(R.id.banner);
-        bl.setOnItemClickListener(new OnItemClickListener() {
-			
+	LinearLayout indexBgLayout = null;
+	ImageButton imageButton1 = null;
+	ImageButton imageButton2 = null;
+	ImageButton imageButton3 = null;
+	ImageButton imageButton4 = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_banner);
+		bl = (BannerLayout) findViewById(R.id.banner);
+		bl.setOnItemClickListener(new OnItemClickListener() {
 			public void onClick(int index, View childview) {
-				// TODO Auto-generated method stub
-				Toast.makeText(getApplicationContext(), "点击了index："+index,Toast.LENGTH_SHORT).show();
+				Toast.makeText(getApplicationContext(), "点击了index：" + index,
+						Toast.LENGTH_SHORT).show();
 			}
 		});
-    }
+		// 获取界面元素的引用
+		indexBgLayout = (LinearLayout) findViewById(R.id.indexbg);
+		imageButton1 = (ImageButton) findViewById(R.id.nav01);
+		imageButton2 = (ImageButton) findViewById(R.id.nav02);
+		imageButton3 = (ImageButton) findViewById(R.id.nav03);
+		imageButton4 = (ImageButton) findViewById(R.id.nav04);
+		// 设置按钮1的消息处理函数
+		imageButton1.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				indexBgLayout.setBackgroundResource(R.drawable.indexbg1);
+			}
+		});
+		// 设置按钮2的消息处理函数
+		imageButton2.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				indexBgLayout.setBackgroundResource(R.drawable.indexbg2);
+			}
+		});
+		// 设置按钮3的消息处理函数
+		imageButton3.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				indexBgLayout.setBackgroundResource(R.drawable.indexbg3);
+			}
+		});
+		// 设置按钮4的消息处理函数
+		imageButton4.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				indexBgLayout.setBackgroundResource(R.drawable.indexbg4);
+			}
+		});
+	}
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.activity_main, menu);
-//        return true;
-//    }
-    
-    @Override
+	@Override
 	protected void onPause() {
 		bl.stopScroll();
 		super.onPause();
@@ -44,18 +73,18 @@ public class BannerActivity extends Activity {
 
 	@Override
 	protected void onRestart() {
-		if(!bl.isScrolling())
+		if (!bl.isScrolling())
 			bl.startScroll();
 		super.onRestart();
 	}
 
 	@Override
 	protected void onResume() {
-		if(!bl.isScrolling())
+		if (!bl.isScrolling())
 			bl.startScroll();
+		indexBgLayout.setBackgroundResource(R.drawable.indexbg);
 		super.onResume();
 	}
-
 
 	@Override
 	protected void onStop() {
