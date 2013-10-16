440c67ca95847e71ba60dd84b88e4fe50ca1ddcb
hewu2008@gmail.com <hewu2008@gmail.com@c1abda00-6e9e-4933-64f5-1a0d4a7cf787>
2013-10-16 13:58:35 +0000

    新增index页

---
+++ Venus_OCR/src/com/wondertek/ocr/IndexActivity.java
@@ -0,0 +1,48 @@
+/**
+ * @author 何武
+ * <br /> 邮箱:hewu@ahmobile.com
+ * <br /> 描述:IndexActivity.java
+ * <br /> 版本：1.0.0
+ * <br /> 日期：2013-10-16
+ */
+package com.wondertek.ocr;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * @author 何武 com.wondrtek.ocr 创建时间：2013-10-16
+ */
+public class IndexActivity extends Activity {
+	protected static final String TAG = "IndexActivity";
+	Handler handler = new Handler();
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(R.layout.activity_index);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Runnable runnable = new Runnable() {
+			@Override
+			public void run() {
+				Log.d(TAG, ">>>run<<<");
+				handler.removeCallbacks(this);
+				Intent intent = new Intent(IndexActivity.this, BannerActivity.class);
+				startActivity(intent);
+				finish();
+			}
+		};
+		handler.postDelayed(runnable, 2000);
+	}
+}
